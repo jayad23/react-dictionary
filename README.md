1dd2096804dd9aa23afb7b5da987af26ede4ddf8
jorgeduje <jorgeduje4@gmail.com>
2023-02-06 14:34:56 -0300

    se adaptan variables de entorno para informacion sensible de firebase, y se agrega README

---
+++ README.md
@@ -0,0 +1,9 @@
+
+
+# Para comenzar, clonar el proyecto e instalar las dependencias
+### `npm install`
+
+## Crear el .env en la raiz del proyecto y adaptar las variables de entorno
+
+## Por ultimo podes levantar el proyecto con
+### `npm start`
--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import {getFirestore} from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAC7bz12SvrhOquJh0oxCQhtPBQ8WMcVoA",
-  authDomain: "dictionary-react-d5aef.firebaseapp.com",
-  projectId: "dictionary-react-d5aef",
-  storageBucket: "dictionary-react-d5aef.appspot.com",
-  messagingSenderId: "517165446613",
-  appId: "1:517165446613:web:9a7e12a618dcbe718bb95c"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAI,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
