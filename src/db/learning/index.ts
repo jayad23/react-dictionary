713b7985c9ccd24464d48d8a6e40ddd67639d8b1
jayad23 <jaidervanegas@live.com>
2023-02-12 00:46:17 +0100

    fix issue with navigation

--- src/db/learning/index.ts
+++ src/db/learning/index.ts
@@ -1,9 +1,11 @@
 import react from "./react.json";
 import webDev from "./development.json";
+import ts from "./typescript.json";
 import library from "./books.json";
 
 export const learning = {
   react,
   webDev,
+  ts,
   library
 };
