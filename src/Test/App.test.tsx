4ff2d45a62c0a3a97b143b5b2acf75ae047f5ea9
jayad23 <jaidervanegas@live.com>
2023-02-12 22:15:21 +0100

    Vitest, Testing library & Happy dom added in

---
+++ src/Test/App.test.tsx
@@ -0,0 +1,29 @@
+import { render, screen, cleanup } from "@testing-library/react";
+import { afterEach, describe, it } from "vitest";
+
+const MockAppComponent = () => {
+  return (
+    <div>
+      <h1>Global</h1>
+      <button>Show</button>
+    </div>
+  )
+}
+
+describe("It tests that all requirements for testing env are synched", () => {
+  afterEach(cleanup);
+
+  it("should log an h1 message in terminal", () => {
+    render(<MockAppComponent />);
+
+    screen.getAllByText("Global");
+  });
+
+  it('should render the button Element', () => {
+    const Element = render(<MockAppComponent />);
+
+    Element.getByRole("button")
+    
+  })
+})
+
